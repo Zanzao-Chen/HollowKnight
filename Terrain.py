3006d48fdefe59d4f5c949a80f6595d5c283cde1
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2023-11-19 13:55:36 -0500

    added terrains

--- Terrain.py
+++ Terrain.py
@@ -1,5 +1,10 @@
 class Terrain:
     def __init__(self, x, y, width, height, type, radius = None):
-        pass 
+        self.x = x
+        self.y = y 
+        self.width = width
+        self.height = height
+        self.type = type
+        self.radius = radius
     def getOutline(x, y, width, height):
         pass
--- hollowKnight.py
+++ hollowKnight.py
@@ -1,15 +1,20 @@
 from cmu_graphics import *
 from Player import *
-
+from Terrain import *
 
 player = Player(100, -200)
+flat1 = Terrain(0, 250, app.width, 50, 'Rectangle')
+flat2 = Terrain(200, 230, app.width, 50, 'Rectangle')
+terrainsList = [flat1, flat2]
 
 def onAppStart(app):
     app.stepsPerSecond = 30
 
 def redrawAll(app):
     drawRect(player.x, -player.y, 20, 50, fill = 'black')
-    print(player.y)
+    for terrain in terrainsList:
+        if terrain.type == 'Rectangle':
+            drawRect(terrain.x, terrain.y, terrain.width, terrain.height, fill = 'green')
 
 def onKeyPress(app, key):
     if key == 'a':
@@ -29,8 +34,6 @@ def onStep(app):
     if player.jumping == True:
         player.timer += 1
         player.jump()
-    
-    
     
 
 def main():
