c200eb3da176546543d44bdd5c2e335489703c2d
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2023-11-21 15:09:47 -0500

    finally working ovals

--- hollowKnight.py
+++ hollowKnight.py
@@ -5,9 +5,9 @@ from Terrain import *
 def onAppStart(app):
     app.stepsPerSecond = 30
 
-player = Player(400, -50, 20, 50)
+player = Player(615, -50, 20, 50)
 flat1 = Terrain(0, 250, app.width, 50, 'Rectangle')
-flat2 = Terrain(200, 230, app.width, 50, 'Rectangle')
+flat2 = Terrain(200, 230, 100, 50, 'Rectangle')
 flat3 = Terrain(0, 230, 50, 50, 'Rectangle')
 oval1 = Terrain(650, 230, 100, 50, 'outerOval')
 terrainsList = [flat1, flat2, flat3, oval1]
@@ -91,6 +91,7 @@ def onStep(app):
                 player.jumping = False
                 player.positions = []
                 player.timer = 0
+                
 
 def checkColliding(terrain, player):
     player.getPlayerVertices()
@@ -126,10 +127,22 @@ def checkCollidingOuterOval(terrain, player):
         player.getPlayerVertices()
         terrain.getTerrainVertices()
         if ((player.orientationX-terrain.x)/(terrain.width/2))**2 + ((player.orientationY-terrain.y)/(terrain.height/2))**2 < 1:
-            return (True, 'down', player.orientationX)
-        else:
-            return False, None, None
-
+            getAngle(terrain, player)
+            player.getPlayerVertices()
+            if ((player.orientationX-terrain.x)/(terrain.width/2))**2 + ((player.orientationY-terrain.y)/(terrain.height/2))**2 < 1:
+                return (True, 'down', player.orientationX)    
+        return False, None, None
+        
+def getAngle(terrain, player):
+    print(player.middleX, player.bottomY)
+    xPartialDerivative =  2*(player.middleX - terrain.x)/((terrain.width/2)**2)
+    yPartialDerivative = 2*(player.bottomY-terrain.y)/((terrain.height/2)**2)
+    alpha = math.atan(xPartialDerivative/yPartialDerivative)*180/math.pi
+    if abs(alpha) < 45:
+        player.rotateAngle = -(alpha)
+    elif abs(alpha) >= 45:
+        player.rotateAngle -(90-alpha)
+    print(alpha, player.rotateAngle, xPartialDerivative, yPartialDerivative)
 def main():
     runApp(width=1000, height=400)
 
