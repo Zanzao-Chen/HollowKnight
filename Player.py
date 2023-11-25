a517fbbe5b1a86bfb3a095d8f8c85fd15bc260f1
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2023-11-25 01:05:48 -0500

    align attack but with slight gaps with positive angles

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 import math
 from cmu_graphics import *
 from Entity import *
+from Vector import *
 
 class Player(Entity):
     def attack(self, upwards=False, downwards=False): 
@@ -15,6 +16,8 @@ class Player(Entity):
                 self.attackDirection = self.direction
                 self.isAttacking = True
                 self.looksAttacking = True
+                if self.isCollidingWithOval == True:
+                    self.alignAttacks()
                 return
             self.attackWidth = self.width*5
             self.attackHeight = self.height
@@ -68,3 +71,105 @@ class Player(Entity):
             enemy.y += enemy.enemyKnockBackDistanceVertical
         elif self.attackDirection == 'down':
             self.isPogoing = True
+    def alignAttacks(self):
+        self.previousAttackTime = app.generalCounter
+        self.attackX = self.x + (self.width if self.direction == 'right' else -self.attackWidth)
+        self.attackY = self.y
+        self.getPlayerVertices()
+        self.cornersRotated = []
+        self.cornersAttack = []
+        self.cornersEnemy = []
+        self.projectedEnemy = []
+        self.projectedAttack = []
+        self.fourPointsAttack1 = []
+        self.fourPointsAttack2 = []
+        self.fourPointsEnemy1 = []
+        self.fourPointsEnemy2 = []
+
+        topY = -self.attackY
+        leftX = self.attackX
+        bottomY = -(self.attackY - self.height)
+        rightX = self.attackX + self.attackWidth
+        middleX = (leftX + rightX)/2
+        middleY = (topY + bottomY)/2
+        attackAngle = self.rotateAngle
+        self.vectorAttackX = Vector(middleX, middleY, attackAngle)
+        self.vectorAttackY = Vector(middleX, middleY, attackAngle+90)
+
+        if self.rotateAngle == 0:
+            shiftAttackX = self.attackWidth/2
+            shiftAttackY = self.attackHeight/2
+            self.vectorAttackRightX = Vector(middleX+shiftAttackX, middleY, attackAngle)
+            self.vectorAttackLeftX = Vector(middleX-shiftAttackX, middleY, attackAngle)
+            self.vectorAttackRightY = Vector(middleX+shiftAttackY, middleY, attackAngle+90)
+            self.vectorAttackLeftY = Vector(middleX-shiftAttackY, middleY, attackAngle+90)
+            self.cornersAttack=[(self.x, -self.y),
+                                (self.attackX, -self.attackY+self.attackHeight),
+                                (self.attackX+self.attackWidth, -self.attackY),
+                                (self.attackX+self.attackWidth, -self.attackY+self.attackHeight)]
+        else:
+            shiftAttackX = (self.attackWidth/2)/(math.cos(attackAngle*math.pi/180)+0.00001)
+            shiftAttackY = (self.attackHeight/2)/(math.sin(attackAngle*math.pi/180)+0.00001)
+            self.vectorAttackRightX = Vector(middleX+shiftAttackX, middleY, attackAngle)
+            self.vectorAttackLeftX = Vector(middleX-shiftAttackX, middleY, attackAngle)
+            self.vectorAttackRightY = Vector(middleX+shiftAttackY, middleY, attackAngle+90)
+            self.vectorAttackLeftY = Vector(middleX-shiftAttackY, middleY, attackAngle+90)
+
+            self.cornersAttack = [
+                (self.vectorAttackLeftX.getIntersection(self.vectorAttackRightY)),
+                (self.vectorAttackLeftX.getIntersection(self.vectorAttackLeftY)),
+                (self.vectorAttackRightX.getIntersection(self.vectorAttackRightY)),
+                (self.vectorAttackRightX.getIntersection(self.vectorAttackLeftY))
+            ]
+        middleX = self.middleX
+        middleY = self.middleY
+        self.rotateAngle
+
+        if self.rotateAngle == 0:
+            pass
+        else:
+            shiftX = (self.width/2)/(math.cos(self.rotateAngle*math.pi/180)+0.00001)
+            shiftY = (self.height/2)/(math.sin(self.rotateAngle*math.pi/180)+0.00001)
+            self.vectorRightX = Vector(middleX+shiftX, middleY, self.rotateAngle)
+            self.vectorLeftX = Vector(middleX-shiftX, middleY, self.rotateAngle)
+            self.vectorRightY = Vector(middleX+shiftY, middleY, self.rotateAngle+90)
+            self.vectorLeftY = Vector(middleX-shiftY, middleY, self.rotateAngle+90)
+
+            self.cornersRotated = [
+                (self.vectorLeftX.getIntersection(self.vectorRightY)),
+                (self.vectorLeftX.getIntersection(self.vectorLeftY)),
+                (self.vectorRightX.getIntersection(self.vectorRightY)),
+                (self.vectorRightX.getIntersection(self.vectorLeftY))
+            ]
+
+        if self.attackDirection == 'right':
+            self.attackPointOfInterest = self.cornersAttack[0]
+            self.originalPointOfInterest = self.cornersRotated[2]
+            (x1, y1) = self.attackPointOfInterest
+            (x2, y2) = self.originalPointOfInterest
+            distance = self.distance(x1, y1, x2, y2)
+            deltaX = math.sin(self.rotateAngle*math.pi/180)*distance
+            deltaY = math.cos(self.rotateAngle*math.pi/180)*distance
+            if self.rotateAngle > 0:
+                self.alignAttackX = -deltaX
+                self.alignAttackY = -deltaY
+            elif self.rotateAngle < 0:
+                self.alignAttackX = -deltaX
+                self.alignAttackY = deltaY
+        elif self.attackDirection == 'left':
+            self.attackPointOfInterest = self.cornersAttack[2]
+            self.originalPointOfInterest = self.cornersRotated[0]
+            (x1, y1) = self.attackPointOfInterest
+            (x2, y2) = self.originalPointOfInterest
+            distance = self.distance(x1, y1, x2, y2)
+            deltaX = math.sin(self.rotateAngle*math.pi/180)*distance
+            deltaY = math.cos(self.rotateAngle*math.pi/180)*distance
+            if self.rotateAngle > 0:
+                self.alignAttackX = deltaX
+                self.alignAttackY = deltaY
+            elif self.rotateAngle < 0:
+                self.alignAttackX = deltaX
+                self.alignAttackY = -deltaY
+        self.attackX += self.alignAttackX
+        self.attackY += self.alignAttackY
+                        
